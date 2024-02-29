11e5f2279cc41b5c4772973004c3a4a118ab8023
Stimoroid <oliverbuus@live.dk>
2024-02-29 09:49:55 +0100

    pis

--- Yatzy.js
+++ Yatzy.js
@@ -1,13 +1,20 @@
-const grids = document.querySelectorAll('grid-item');
 const labels = ["1-s", "2-s", "3-s", "4-s", "5-s", "6-s",
                 "One pair", "Two pairs", "Three same",
                 "Four same", "Full house", "Small straight",
                 "Large straight", "Chance", "Yatzy"];
 const inputBoxes = [];
 
+let gridDice = document.getElementById('grid-dice');
+let gridPoints = document.getElementById('grid-points');
+
+
+
 for (let i = 0; i < labels.length; i++) {
+    let label = document.createElement('label');
+    label.textContent = labels[i];
+    gridPoints.appendChild(label);
     let input = document.createElement('input');
     input.id = 'input' + i;
     inputBoxes[i] = input;
-    grids[1].appendChild(input);
+    gridPoints.appendChild(input);
 }
